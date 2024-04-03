5af900d5427815d70aa7d837ac46e1481fa841f0
hoseinlook <hoseinlooki0@gmail.com>
2024-04-03 15:13:06 +0330

    Add a sentiment Model

--- spark_sentiment_app/sentiment_transformer.py
+++ spark_sentiment_app/sentiment_transformer.py
@@ -1,23 +1,22 @@
-import random
 from typing import Iterator
 
 import pandas as pd
 from pyspark.sql.functions import pandas_udf
-from pyspark.sql.types import LongType
+from pyspark.sql.types import LongType ,DoubleType
+from transformers import pipeline
 
 
 class SentimentModel:
     def __init__(self):
-        pass
+        self.model = pipeline("sentiment-analysis")
 
     def predict(self, text: str) -> int:
-        return random.randint(-1, 1)
+        return self.model.predict([text])[0]["score"]
 
 
-@pandas_udf(LongType(), None)
+@pandas_udf(DoubleType(), None)
 def sentiment_calculate_udf(iterator: Iterator[pd.Series]) -> Iterator[pd.Series]:
     model = SentimentModel()
     for x in iterator:
-
         # Use that state for whole iterator.
         yield x.apply(model.predict)
